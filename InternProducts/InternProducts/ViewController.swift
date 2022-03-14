9f947c884ec59091f4c556f8ab229ebc2f50400d
Cristina Cernat <cernat.cristina@yahoo.com>
2022-03-14 21:39:50 +0200

    low-level backend communication

--- InternProducts/InternProducts/ViewController.swift
+++ InternProducts/InternProducts/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let session = URLSession(configuration: .default)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+        let url = URL(string: "http://localhost:8080/login?username=admin&password=1234")!
+        // MARK: - treat the request
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Request error. Received: \(error)")
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                print("Request error, no 200 status. Received: \(response.statusCode)")
+            } else if let data = data {
+                // MARK: - process data
+                print(data)
+            } else {
+                print("Request error, unexpected condition")
+            }
+        }
+
+        dataTask.resume()
     }
 
 
