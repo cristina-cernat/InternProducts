30ba6c17ba5d4e5e0961f02c90d3ed8e2940c8ac
Cristina Cernat <cernat.cristina@yahoo.com>
2022-03-18 00:41:38 +0200

    products request
    bad hardcoded url

--- InternProducts/InternProducts/ProductsViewController.swift
+++ InternProducts/InternProducts/ProductsViewController.swift
@@ -16,16 +16,43 @@ class ProductsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var products = [[String: Any]]()
 
+    let session = URLSession(configuration: .default)
+    var dictionary = [String:Any]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "ProductTableViewCell", bundle: nil), forCellReuseIdentifier: Constant.productCellIdentifier)
 
-        //self.tableView.delegate = self
-       // self.tableView.dataSource = self
+        let url = URL(string: "http://localhost:8080/products?loginToken=668961808.772846")!
+        // MARK: - treat the request
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Request error. Received: \(error)")
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                print("Request error, no 200 status. Received: \(response.statusCode)")
+            } else if let data = data {
+                // MARK: - process data
+                do {
+                       guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                           print("Data serialization error: Unexpected data format")
+                           return
+                       }
 
-       // self.view.addSubview(tableView)
+                       self.dictionary = jsonObject
+                       print(self.dictionary)
 
-    }
+                   } catch {
+                       print("Data serialization error: \(error)")
+                   }
+            } else {
+                print("Request error, unexpected condition")
+            }
+        }
+
+        dataTask.resume()
+     }
+
+
     
 
     /*
